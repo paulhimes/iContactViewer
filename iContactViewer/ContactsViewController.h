fd07339f96bfa0130e3ef7671c0b3e89906dce9b
Paul Himes <paul@paulhimes.com>
2013-03-07 23:32:56 -0600

    Initial commit.

---
+++ iContactViewer/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  iContactViewer
+//
+//  Created by Paul Himes on 3/7/13.
+//  Copyright (c) 2013 MSSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UITableViewController
+
+@end
